8a4749fe17329e3abb70bd4934cec4c66e5e963c
David Wu <david@wu-man.com>
2012-02-18 20:53:09 +0800

    test

--- src/org/hermitcrab/ui/AppDetailFragment.java
+++ src/org/hermitcrab/ui/AppDetailFragment.java
@@ -1,7 +1,20 @@
 package org.hermitcrab.ui;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class AppDetailFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(android.R.layout.simple_list_item_1,
+				container, false);
+		view.setVisibility(View.GONE);
+		return view;
+	}
 
 }
